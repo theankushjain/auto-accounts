49c9ebea5b293eb6e0799bff105ed1dc212bad52
Ankush Jain <ankushjainr@gmail.com>
2019-02-06 17:28:59 +0530

    authentication frontend completed

---
+++ src/components/company/AddCompany.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">Here we can add a new company</div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">This is the HomePage</div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
